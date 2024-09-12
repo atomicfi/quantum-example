4b41f72c78dd4072e17819bca92f4617e96d484c
Sean Hill <sean.daryl.hill@gmail.com>
2024-09-12 15:25:54 -0700

    chore: add docs of quantum parameters

--- ios/quantum-example/ViewController.swift
+++ ios/quantum-example/ViewController.swift
@@ -18,8 +18,12 @@ class ViewController: UIViewController {
 	
 	override func viewDidLoad() {
 		super.viewDidLoad()
-		// Do any additional setup after loading the view.
 		
+        // Quantum takes two parameters.
+        // 1. A WebView that uses the qauntum-js package to manage browsers (see example webapp)
+        // 2. A UIViewController to present the browsers on top of
+        // The WebView could be an existing web application you present to your users inside of your native app
+        // or a hidden WebView that is used to house a web application that uses quantum-js to manage browsers
 		quantum = Quantum(view: webView, controller: self)
 		webView.load(URLRequest(url: URL(string: "http://localhost:3000")!))
 	}
