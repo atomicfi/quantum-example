b5aec582b8440cef49456c557546f4b2d3a4b5d6
Sean Hill <sean.daryl.hill@gmail.com>
2024-09-12 14:10:08 -0700

    feat: authenticate using quantum

--- webapp/src/App.tsx
+++ webapp/src/App.tsx
@@ -1,14 +1,11 @@
-import React, { useEffect } from 'react'
+import React, { useState } from 'react'
 import './App.css'
 import { Quantum } from '@atomicfi/quantum-js'
 
 function App() {
-  useEffect(() => {
-    initializeQuantum()
-  }, [])
+  const [authenticated, setAuthenticated] = useState(false)
 
   async function initializeQuantum() {
-    // Authenticate into a website
     const { page } = await Quantum.launch()
     const startURL = 'https://mocky.atomicfi.com'
 
@@ -19,16 +16,75 @@ function App() {
       return !!url?.includes('/profile')
     })
 
+    setAuthenticated(true)
+
     await page.hide()
   }
 
   return (
-    <div className="App">
-      <header className="App-header">
-        <p>Example QuantumJS application</p>
-      </header>
+    <div className="App" style={appStyle}>
+      {!authenticated ? (
+        <div className="Launch" style={launchStyle}>
+          <header className="App-header">
+            <p>Example using QuantumJS</p>
+            <small>Use `test-good` for your username to authenticate.</small>
+          </header>
+          <button onClick={initializeQuantum} style={buttonStyle}>
+            Launch
+          </button>
+        </div>
+      ) : (
+        <div className="Authenticated" style={authenticatedStyle}>
+          <p>You are authenticated!</p>
+          <p>
+            Please refer to the{' '}
+            <a
+              href="https://github.com/atomicfi/quantum-js"
+              target="_blank"
+              style={linkStyle}
+            >
+              QuantumJS
+            </a>{' '}
+            documentation for further usage.
+          </p>
+        </div>
+      )}
     </div>
   )
+}
+
+const appStyle = {
+  height: '100vh',
+  background: 'linear-gradient(#111, #555)',
+  color: 'white'
+}
+
+const launchStyle = {
+  display: 'flex',
+  flexDirection: 'column' as 'column',
+  justifyContent: 'center',
+  alignItems: 'center',
+  height: '100vh',
+  textAlign: 'center' as 'center'
+}
+
+const buttonStyle = {
+  backgroundColor: '#3f51b5',
+  color: 'white',
+  padding: '10px 20px',
+  border: 'none',
+  borderRadius: '4px',
+  cursor: 'pointer',
+  fontSize: '16px',
+  marginTop: '20px'
+}
+
+const authenticatedStyle = {
+  padding: 20
+}
+
+const linkStyle = {
+  color: '#ADD8E6'
 }
 
 export default App
